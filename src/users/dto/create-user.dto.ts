ae65ae6fafd9d65479a00dafbf0bd4fb941dacb5
Ekaterina696 <katya.osacha@mail.ru>
2024-06-25 16:23:27 +0300

    добавлена валидация

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,15 @@
+import * as Joi from 'joi';
+
 export class CreateUserDto {
   firstName: string;
   lastName: string;
   email: string;
   password: string;
 }
+
+export const CreateUserSchema = Joi.object({
+  first_name: Joi.string().required(),
+  last_name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required().min(6)
+});
